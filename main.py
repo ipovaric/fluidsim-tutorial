492b21fe70b8f6067379141d83686b114b7f90fe
Igor <ipovaric@gmail.com>
2026-09-01 20:40:32 -0600

    working more on sim creation

--- main.py
+++ main.py
@@ -4,24 +4,19 @@ import fluidsim
 import shutil
 
 from fluidsim.solvers.ns2d.solver import Simul
+from fluidsim import load_sim_for_plot
+from fluidsim import load_state_phys_file
+
 params = Simul.create_default_params()
-
-
-# print([attr for attr in dir(params) if not attr.startswith("_")])
-# print(type(params.nu_2))
-# print(type(params.output))
-# print(params)
-
-params.nu_2 = 1e-3
-params.forcing.enable = False
-
-params.init_fields.type = "noise"
-params.output.periods_save.spatial_means = 1.0
-
 
 sim = Simul(params)
 
 # sim.time_stepping.start()
+
+sim = load_state_phys_file(r"C:\\Users\\ipova\\Sim_data\\NS2D_48x48_S8x8_2026-09-01_20-36-27")
+
+# sim.output.spatial_means.plot()
+sim.output.phys_fields.plot()
 
 # shutil.rmtree(sim.output.path_run)
 
